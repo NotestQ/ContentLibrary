a1bbd5bce45b28aa0b075e2dcc7bb98cedefe89c
unknown <noccqlive@gmail.com>
2024-04-12 01:27:35 -0300

    File didn't go along. This should really be in the main class as it only adds one (1) method but it's ok

---
+++ TypeSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Collections;
+using Zorro.Core.Serizalization;
+
+namespace ContentLibrary
+{
+    internal class TypeSerializer : BinarySerializer
+    {
+        public void WriteShort(short value)
+        {
+            NativeArray<short> nativeArray = new NativeArray<short>(1, Allocator.Temp, NativeArrayOptions.ClearMemory);
+            nativeArray[0] = value;
+            this.WriteBytes(nativeArray.Reinterpret<byte>(UnsafeUtility.SizeOf<short>()));
+            nativeArray.Dispose();
+        }
+    }
+}
