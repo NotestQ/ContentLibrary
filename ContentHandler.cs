242482c53904b37f8ea2221452f5c43816efa4c0
unknown <noccqlive@gmail.com>
2024-04-17 00:16:00 -0300

    Remove using references that are inutilized

--- ContentHandler.cs
+++ ContentHandler.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mime;
 using MyceliumNetworking;
 using Photon.Pun;
-using Sirenix.Serialization;
 using Steamworks;
-using UnityEngine.Rendering;
 
 namespace ContentLibrary
 {
--- EmptyContentEvent.cs
+++ EmptyContentEvent.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Zorro.Core.Serizalization;
 
 namespace ContentLibrary
