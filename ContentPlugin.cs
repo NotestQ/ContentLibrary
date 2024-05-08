b881ab4a1f0c3f2258abe32923f72558381ff82d
unknown <noccqlive@gmail.com>
2024-05-07 21:55:08 -0300

    fix: Clear EventList on LobbyLeft

--- ContentPlugin.cs
+++ ContentPlugin.cs
@@ -38,6 +38,7 @@ namespace ContentLibrary
 
             MyceliumNetwork.RegisterNetworkObject(this, modID);
             MyceliumNetwork.LobbyEntered += ContentHandler.OnLobbyEntered;
+            MyceliumNetwork.LobbyLeft += ContentHandler.OnLobbyLeft;
 
             Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
         }
